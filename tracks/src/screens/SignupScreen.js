dea471ab2c38852240c437cdb63b52190fe3039c
Nick Hardwick <nickghardwick@gmail.com>
2020-03-24 17:47:24 -0700

    Navigation wired up

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Button} from 'react-native';
 
-let SignupScreen = function() {
-    return <Text style={{fontSize: 48}}>SignupScreen</Text>;
+let SignupScreen = function({navigation}) {
+    return (
+        <>
+            <Text style={{fontSize: 48}}>Sign Up Screen</Text>
+            <Button
+                title="Go to Sign In"
+                onPress={() => navigation.navigate('Signin')}
+            />
+            <Button
+                title="Go to Main Flow"
+                onPress={() => navigation.navigate('mainFlow')}
+            />
+        </>
+    );
 };
 
 const styles = StyleSheet.create({});
--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Button} from 'react-native';
 
-let TrackListScreen = function() {
-    return <Text style={{fontSize: 48}}>TrackListScreen</Text>;
+let TrackListScreen = function({navigation}) {
+    return (
+        <>
+            <Text style={{fontSize: 48}}>TrackListScreen</Text>
+            <Button 
+                title="Go to Track Detail"
+                onPress={() => navigation.navigate('TrackDetail')}
+            />
+        </>
+    );
 };
 
 const styles = StyleSheet.create({});
