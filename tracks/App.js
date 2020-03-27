a6449a56e2c9a9edb57d7bce5fda05068aa934c8
Nick Hardwick <nickghardwick@gmail.com>
2020-03-27 16:49:42 -0700

    Auth Context created and updated

--- tracks/App.js
+++ tracks/App.js
@@ -8,6 +8,7 @@ import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
+import {Provider as AuthProvider} from './src/context/AuthContext';
 
 let switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -24,4 +25,12 @@ let switchNavigator = createSwitchNavigator({
   })
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+};
