2e09e974147eb36da0f9b4cf0d922bd01b85492b
Nick Hardwick <nickghardwick@gmail.com>
2020-03-28 16:25:27 -0700

    Saving signed in user token

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import {AsyncStorage} from 'react-native';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
 
@@ -5,6 +6,8 @@ const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
             return {...state, errorMessage: action.payload};
+        case 'signup':
+            return {errorMessage: '', token: action.payload};
         default:
             return state;
     }
@@ -14,7 +17,8 @@ const signup =  function(dispatch) {
     return async ({email, password}) => {
         try {
             const response = await trackerApi.post('/signup', {email, password});
-            console.log(response.data);
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({type: 'signup', payload: response.data.token});
         } catch (e) {
             dispatch({type: 'add_error', payload: 'Something went wrong with sign up'})
         }
@@ -36,5 +40,5 @@ const signout = function(dispatch) {
 export const {Provider, Context} = createDataContext(
     authReducer,
     {signin, signout, signup},
-    {isSignedIn: false, errorMessage: ''}
+    {token: null, errorMessage: ''}
 );
