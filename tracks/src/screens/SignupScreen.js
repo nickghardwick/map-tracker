08395081d7259f64a5af50b7fec8c559623b6c04
Nick Hardwick <nickghardwick@gmail.com>
2020-03-28 17:06:14 -0700

    Link to Signin screen added

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import {Text, Input, Button} from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import {Context as AuthContext} from '../context/AuthContext';
@@ -34,6 +34,12 @@ let SignupScreen = function({navigation}) {
             <Spacer>
             <Button title="Sign Up" onPress={() => signup({email, password})} />
             </Spacer>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Spacer>
+                <Text style={styles.link}>Already have an account? Sign in instead.</Text>
+                </Spacer>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -55,6 +61,9 @@ let styles = StyleSheet.create({
         color: 'red',
         marginLeft: 15,
         marginTop: 15,
+    },
+    link: {
+        color: 'blue',
     },
 });
 
