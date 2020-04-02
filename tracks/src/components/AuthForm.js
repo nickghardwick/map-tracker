0c48ae2fcef90ef8b5d2c203e7472f50c4645556
Nick Hardwick <nickghardwick@gmail.com>
2020-04-01 19:30:26 -0700

    Extracted form logic refactor

---
+++ tracks/src/components/AuthForm.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {Text, Button, Input} from 'react-native-elements';
+import Spacer from './Spacer'
+
+const AuthForm = ({headerText, errorMessage, onSubmit, submitButtonText}) => {
+    let [email, setEmail] = useState('');
+    let [password, setPassword] = useState('');
+
+    return (
+        <>
+        <Spacer>
+            <Text h3>{headerText}}</Text>
+        </Spacer>
+        <Input
+            label="Email"
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
+            autoCorrect={false}
+        />
+        <Spacer />
+        <Input
+            label="Password"
+            value={password}
+            onChangeText={setPassword}
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry
+        />
+        {errorMessage ? <Text style={styles.errorMessage}>{errorMessage}</Text> : null}
+        <Spacer>
+            <Button title={submitButtonText} onPress={() => onSubmit({email, password})} />
+        </Spacer>
+        </>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    errorMessage: {
+        fontSize: 16,
+        color: 'red',
+        marginLeft: 15,
+        marginTop: 15,
+    }
+});
+
+export default AuthForm;
--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -3,38 +3,18 @@ import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import {Text, Input, Button} from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import {Context as AuthContext} from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
 
 let SignupScreen = function({navigation}) {
     let {state, signup} = useContext(AuthContext);
-    let [email, setEmail] = useState('');
-    let [password, setPassword] = useState('');
-
     return (
         <View style={styles.container}>
-            <Spacer>
-            <Text h3>Sign Up for Tracker!</Text>
-            </Spacer>
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-                autoCorrect={false}
+            <AuthForm
+                headerText="Sign Up for Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={({email, password}) => signup({email, password})}
             />
-            <Spacer />
-            <Input
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCapitalize="none"
-                autoCorrect={false}
-                secureTextEntry
-            />
-            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
-            <Spacer>
-            <Button title="Sign Up" onPress={() => signup({email, password})} />
-            </Spacer>
-
             <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
                 <Spacer>
                 <Text style={styles.link}>Already have an account? Sign in instead.</Text>
@@ -55,12 +35,6 @@ let styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 250,
-    },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15,
     },
     link: {
         color: 'blue',
