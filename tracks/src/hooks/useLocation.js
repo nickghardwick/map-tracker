731b7ac915807b22bd8068e3ee56b041f9c91406
Nick Hardwick <nickghardwick@gmail.com>
2020-04-06 12:03:30 -0700

    Extracted location gathering into hook

---
+++ tracks/src/hooks/useLocation.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-location';
+
+export default (callback) => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+      await watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10,
+        }, callback
+      );
+    } catch (err) {
+      setErr(err);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return [err];
+};
--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,34 +1,15 @@
 import '../_mockLocation';
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet} from 'react-native';
 import {Text} from 'react-native-elements';
 import {SafeAreaView} from 'react-navigation';
-import {requestPermissionsAsync, watchPositionAsync, Accuracy} from 'expo-location';
 import Map from '../components/Map';
 import {Context as LocationContext} from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
 let TrackCreateScreen = function() {
     const {addLocation} = useContext(LocationContext);
-    const [err, setErr] = useState(null);
-
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync();
-            await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000,
-                distanceInterval: 10,
-            }, (location) => {
-                addLocation(location);
-            });
-        } catch (err) {
-            setErr(err);
-        }
-    };
-
-    useEffect(() => {
-        startWatching();
-    }, []);
+    const [err] = useLocation(addLocation);
 
     return (
         <SafeAreaView forceInset={{top: 'always'}}>
