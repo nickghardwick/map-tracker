d4453e368ae6c7bad801af2f76630eb5dbcbb481
Nick Hardwick <nickghardwick@gmail.com>
2020-04-06 11:47:40 -0700

    FIXED: Scale factor of _mockLocation

--- tracks/src/_mockLocation.js
+++ tracks/src/_mockLocation.js
@@ -11,8 +11,8 @@ const getLocation = increment => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment + tenMetersWithDegrees,
-      latitude: 37.33233141 + increment + tenMetersWithDegrees,
+      longitude: -122.0312186 + increment * tenMetersWithDegrees,
+      latitude: 37.33233141 + increment * tenMetersWithDegrees,
     }
   };
 };
