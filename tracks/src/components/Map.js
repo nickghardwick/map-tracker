85d90d9d57d8d022d800315693a2b1ff484fcdd3
Nick Hardwick <nickghardwick@gmail.com>
2020-04-09 19:24:34 -0700

    Track created when recording

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -4,7 +4,7 @@ import MapView, {Polyline, Circle} from 'react-native-maps';
 import {Context as LocationContext} from '../context/LocationContext';
 
 const Map = () => {
-  const {state: {currentLocation}} = useContext(LocationContext);
+  const {state: {currentLocation, locations}} = useContext(LocationContext);
 
   if (!currentLocation) {
     return <ActivityIndicator size="large" style={{marginTop: 200}}/>;
@@ -24,6 +24,9 @@ const Map = () => {
         radius={30}
         strokeColor="rgba(158, 158, 255, 1.0)"
         fillColor="rgba(158, 158, 255, 0.3)"
+      />
+      <Polyline
+        coordinates={locations.map(loc => loc.coords)}
       />
     </MapView>
   );
