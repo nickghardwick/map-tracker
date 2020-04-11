e928d682d60161546a3117601dc626631ea58855
Nick Hardwick <nickghardwick@gmail.com>
2020-04-10 17:24:36 -0700

    Reset form, navigate back to list after save

--- tracks/src/hooks/useSaveTrack.js
+++ tracks/src/hooks/useSaveTrack.js
@@ -1,13 +1,16 @@
 import {useContext} from 'react';
 import {Context as TrackContext} from '../context/TrackContext';
 import {Context as LocationContext} from '../context/LocationContext';
+import {navigate} from '../navigationRef';
 
 export default () => {
   const {createTrack} = useContext(TrackContext);
-  const {state: {locations, name}} = useContext(LocationContext);
+  const {state: {locations, name}, reset} = useContext(LocationContext);
 
-  const saveTrack = () => {
-    createTrack(name, locations);
+  const saveTrack = async () => {
+    await createTrack(name, locations);
+    reset();
+    navigate('TrackList');
   };
 
   return [saveTrack];
