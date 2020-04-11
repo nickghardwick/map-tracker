919470b1d6ebde725b85b0a3c40a4f82f7caac32
Nick Hardwick <nickghardwick@gmail.com>
2020-04-10 17:13:59 -0700

    Sends recorded track to database via api

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import {AsyncStorage} from 'react-native';
 
-export default axios.create({
-    baseURL: 'http://5128eab1.ngrok.io'
+const instance = axios.create({
+    baseURL: 'http://46af8f8a.ngrok.io'
 });
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
+    },
+);
+
+export default instance;
--- tracks/src/context/TrackContext.js
+++ tracks/src/context/TrackContext.js
@@ -1,18 +1,19 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const trackReducer = (state, action) => {
     switch (action.type) {
         default:
             return state;
     }
-}
+};
 
 const fetchTracks = dispatch => () => {
 
 };
 
-const createTrack = dispatch => (name, locations) => {
-    console.log(name, locations.length);
+const createTrack = dispatch => async (name, locations) => {
+    await trackerApi.post('/tracks', {name, locations});
 };
 
 export const {Provider, Context} = createDataContext(
