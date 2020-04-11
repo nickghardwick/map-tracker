105d37dcb8c3d42926e89169b77f02222c2d93db
Nick Hardwick <nickghardwick@gmail.com>
2020-04-11 16:53:29 -0700

    Show saved tracks from database on TrackListScreen

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {AsyncStorage} from 'react-native';
 
 const instance = axios.create({
-    baseURL: 'http://46af8f8a.ngrok.io'
+    baseURL: 'http://7051aec8.ngrok.io'
 });
 
 instance.interceptors.request.use(
--- tracks/src/context/TrackContext.js
+++ tracks/src/context/TrackContext.js
@@ -1,15 +1,19 @@
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
+import { AsyncStorage, ActionSheetIOS } from 'react-native';
 
 const trackReducer = (state, action) => {
     switch (action.type) {
+        case 'fetch_tracks':
+            return action.payload;
         default:
             return state;
     }
 };
 
-const fetchTracks = dispatch => () => {
-
+const fetchTracks = dispatch => async () => {
+    const response = await trackerApi.get('/tracks');
+    dispatch({type: 'fetch_tracks', payload: response.data});
 };
 
 const createTrack = dispatch => async (name, locations) => {
